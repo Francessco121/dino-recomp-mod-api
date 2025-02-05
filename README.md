95716bafebd79daf0b5784a746c25125a3ee565c
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-02-04 22:06:51 -0500

    Add modding API stuff

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Dinosaur Planet: Recompiled Symbols
+# Dinosaur Planet: Recompiled Mod API
+This repository contains everything necessary to create mods for [Dinosaur Planet: Recompiled](https://github.com/Francessco121/dino-recomp), including:
+
+- Symbol definitions
+- Decompilation headers
+- Recomp modding headers
+- Recomp-specific headers for Dinosaur Planet DLLs
+
+## Regenerating Symbols
+When updating to a new version of the decompilation, the symbol TOML files and DLL headers must be regenerated.
+
+### 1. Build the Decompilation Project
+Follow the build instructions in [the decompilation readme](./dinosaur-planet/README.md). An up-to-date `build/dino.elf` file is required for the next step.
+
+### 2. Regenerate Symbols
+Run the following tool (from this directory) to regenerate symbols and recomp DLL headers:
+```bash
+python ./dinosaur-planet/tools/gen_recomp_syms.py
+```
---
+++ include/imports.h
@@ -0,0 +1,41 @@
+#ifndef __IMPORTS_H__
+#define __IMPORTS_H__
+
+#include <PR/ultratypes.h>
+#include "modding.h"
+#include "stdarg.h"
+
+RECOMP_IMPORT("*", int sprintf(char *s, const char *fmt, ...))
+RECOMP_IMPORT("*", int vsprintf(char *s, const char *fmt, va_list args))
+
+RECOMP_IMPORT("*", const char *recomp_vsprintf_helper(const char *fmt, va_list args))
+RECOMP_IMPORT("*", const char *recomp_sprintf_helper(const char *fmt, ...))
+
+RECOMP_IMPORT("*", s32 mod_dbgui_begin(const char *str, s32 *open))
+RECOMP_IMPORT("*", void mod_dbgui_end())
+RECOMP_IMPORT("*", void mod_dbgui_text(const char *text))
+RECOMP_IMPORT("*", void mod_dbgui_label_text(const char *label, const char *text))
+RECOMP_IMPORT("*", void mod_dbgui_same_line());
+RECOMP_IMPORT("*", s32 mod_dbgui_begin_combo(const char *label, const char *preview))
+RECOMP_IMPORT("*", void mod_dbgui_end_combo())
+RECOMP_IMPORT("*", s32 mod_dbgui_selectable(const char *label, s32 selected))
+RECOMP_IMPORT("*", s32 mod_dbgui_button(const char *label))
+RECOMP_IMPORT("*", s32 mod_dbgui_is_open())
+RECOMP_IMPORT("*", s32 mod_dbgui_begin_main_menu_bar())
+RECOMP_IMPORT("*", void mod_dbgui_end_main_menu_bar())
+RECOMP_IMPORT("*", s32 mod_dbgui_begin_menu(const char *label))
+RECOMP_IMPORT("*", void mod_dbgui_end_menu())
+RECOMP_IMPORT("*", s32 mod_dbgui_menu_item(const char *label, s32 *selected))
+RECOMP_IMPORT("*", s32 mod_dbgui_collapsing_header(const char *label))
+RECOMP_IMPORT("*", s32 mod_dbgui_tree_node(const char *label))
+RECOMP_IMPORT("*", void mod_dbgui_tree_pop())
+RECOMP_IMPORT("*", s32 mod_dbgui_begin_child(const char *str_id))
+RECOMP_IMPORT("*", void mod_dbgui_end_child())
+RECOMP_IMPORT("*", s32 mod_dbgui_checkbox(const char *label, s32 *value))
+RECOMP_IMPORT("*", s32 mod_dbgui_input_int(const char *label, s32 *value))
+RECOMP_IMPORT("*", s32 mod_dbgui_input_float(const char *label, f32 *value))
+
+RECOMP_IMPORT("*", void mod_dbgui_textf(const char *fmt, ...))
+RECOMP_IMPORT("*", void mod_dbgui_label_textf(const char *label, const char *fmt, ...))
+
+#endif
