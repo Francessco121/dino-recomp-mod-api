1aba2ee49f9490b2d4e2ec937b91dabc758567aa
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-02-08 19:21:27 -0500

    Update recomp imports

--- include/imports.h
+++ include/imports.h
@@ -5,6 +5,10 @@
 #include "modding.h"
 #include "stdarg.h"
 
+typedef struct {
+    f32 x, y;
+} DbgUiPos;
+
 RECOMP_IMPORT("*", int sprintf(char *s, const char *fmt, ...))
 RECOMP_IMPORT("*", int vsprintf(char *s, const char *fmt, va_list args))
 
@@ -37,6 +41,73 @@ RECOMP_IMPORT("*", void recomp_dbgui_end_child())
 RECOMP_IMPORT("*", s32 recomp_dbgui_checkbox(const char *label, s32 *value))
 RECOMP_IMPORT("*", s32 recomp_dbgui_input_int(const char *label, s32 *value))
 RECOMP_IMPORT("*", s32 recomp_dbgui_input_float(const char *label, f32 *value))
+RECOMP_IMPORT("*", s32 recomp_dbgui_input_text(const char *label, char *buffer, s32 buffer_size))
+RECOMP_IMPORT("*", void recomp_dbgui_push_str_id(const char *str_id))
+RECOMP_IMPORT("*", void recomp_dbgui_pop_id())
+RECOMP_IMPORT("*", s32 recomp_dbgui_is_item_hovered());
+RECOMP_IMPORT("*", void recomp_dbgui_get_display_size(f32 *width, f32 *height))
+typedef struct {
+    float r;
+    float g;
+    float b;
+    float a;
+} DbgUiColor;
+RECOMP_IMPORT("*", u32 recomp_dbgui_color_float4_to_u32(const DbgUiColor *color))
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_text(const DbgUiPos *pos, u32 color, const char *text))
+typedef struct {
+    DbgUiPos p1;
+    DbgUiPos p2;
+    u32 color;
+    float thickness;
+} DbgUiLine;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_line(const DbgUiLine *line))
+typedef struct {
+    DbgUiPos center;
+    float radius;
+    u32 color;
+    s32 numSegments;
+    float thickness;
+} DbgUiCircle;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_circle(const DbgUiCircle *circle))
+typedef struct {
+    DbgUiPos center;
+    float radius;
+    u32 color;
+    s32 numSegments;
+} DbgUiCircleFilled;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_circle_filled(const DbgUiCircleFilled *circle))
+typedef struct {
+    DbgUiPos center;
+    float radiusX;
+    float radiusY;
+    u32 color;
+    float rotation;
+    s32 numSegments;
+    float thickness;
+} DbgUiEllipse;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_ellipse(const DbgUiEllipse *ellipse))
+typedef struct {
+    DbgUiPos center;
+    float radiusX;
+    float radiusY;
+    u32 color;
+    float rotation;
+    s32 numSegments;
+} DbgUiEllipseFilled;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_ellipse_filled(const DbgUiEllipseFilled *ellipse))
+typedef struct {
+    DbgUiPos min;
+    DbgUiPos max;
+    u32 color;
+    float thickness;
+} DbgUiRect;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_rect(const DbgUiRect *rect))
+typedef struct {
+    DbgUiPos min;
+    DbgUiPos max;
+    u32 color;
+} DbgUiRectFilled;
+RECOMP_IMPORT("*", void recomp_dbgui_foreground_rect_filled(const DbgUiRectFilled *rect))
 
 RECOMP_IMPORT("*", void recomp_dbgui_textf(const char *fmt, ...))
 RECOMP_IMPORT("*", void recomp_dbgui_label_textf(const char *label, const char *fmt, ...))
